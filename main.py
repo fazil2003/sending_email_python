c25d28f94f4ccf87c3ffb4766f82b8ff13c12861
Mohamed Fazil <65252590+fazil2003@users.noreply.github.com>
2020-08-27 20:19:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#import required modules 
+import smtplib 
+  
+# list of email_id to send the mail 
+li = ["test@gmail.com","test2@gmail.com] 
+  
+for dest in li: 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    s.starttls() 
+    s.login("your_mail@gmail.com", "your_password") 
+    message = "Message_you_need_to_send"
+    s.sendmail("sender_email_id", dest, message) 
+    s.quit() 
